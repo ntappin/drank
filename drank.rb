58cf2f1622b2ea80c03196be950f8ee9a339d26f
Ben Cox <benjamin.cox@jadedpixel.com>
2014-07-18 13:04:33 -0400

    Initial

---
+++ drank.rb
@@ -0,0 +1,51 @@
+require 'twitter'
+require 'io/console'
+load 'creds.rb'
+
+@queue = []
+@twitter
+
+def stdin_to_person(input)
+  case input
+  when 'b'
+    "Ben"
+  when 'n'
+    "Nathan"
+  when 'l'
+    "Lydia"
+  when 'a'
+    "Alex"
+  when 'h'
+    "Hugh"
+  when 'd'
+    "David"
+  when 'w'
+    "Wilson"
+  when 'm'
+    "Mitch"
+  when 's'
+    "Massad"
+  else
+    "Someone"
+  end
+end
+
+def tweet_queue(drank)
+  @queue.push(drank)
+end
+
+def tweeter(person)
+  puts person
+end
+
+loop do
+
+  input = STDIN.getch
+  if input == "\\"
+    break
+  end
+
+  tweet_queue(input)
+  tweeter(stdin_to_person(@queue.pop))
+
+end
